235ee54043e63f3fea6e1249c4557df0ce9eadde
Nicole Vella <nicolevella@OCADUs-MacBook-Pro.local>
2026-05-27 12:03:30 -0400

    add dist folder for github pages

---
+++ dist/assets/motion-c5d33f45.js
@@ -0,0 +1,2 @@
+import{c as e,j as r,C as s,r as o,A as t}from"./App-c4093aba.js";const a=e.createRoot(document.querySelector("#root"));a.render(r.jsx(s,{flat:!0,shadows:!0,camera:{fov:45,near:.1,far:200,position:[0,0,20]},children:r.jsx(o.Suspense,{children:r.jsx(t,{})})}));
+//# sourceMappingURL=motion-c5d33f45.js.map
